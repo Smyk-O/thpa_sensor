cc011048e4f6b90bd730f4ab735a88957109e35f
Smyk-O <smyk006@yandex.ru>
2018-09-16 04:37:22 +0300

    1.0

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ $(document).ready(function () {
 
     setInterval(function () {
         var now = new Date;
-        var clock = $('.time');
+        var $clock = $('.time');
+        var $date = $('.col');
         // дата и время
-        now = now.getHours() + ':' + now.getMinutes();
+        clok_data = now.toLocaleString("ru", { hour: '2-digit', minute: '2-digit' });
+        week_data = now.toLocaleString("en-US", { weekday: 'long' });
+        month_data = now.toLocaleString("en-US", { month: 'long', day: '2-digit' });
+
         // показания счетчиков
         fetch('https://api.waqi.info/api/feed/@8641/obs.en.json')
             .then(res => res.json())
@@ -13,15 +17,18 @@ $(document).ready(function () {
         fetch('https://api.thingspeak.com/channels/266210/feeds.json')
             .then(res => res.json())
             .then(json => thp_sens = json.feeds[99])
+        var tem_sens = thp_sens.field1
+        var hum_sens = thp_sens.field2
+        var press_sens = thp_sens.field3
 
-            // field1:"Temperature"
-            // field2:"Humidity"
-            // field3:"Pressure"
 
-        clock.html(now);
+        $clock.html(clok_data);
+        $date.html(week_data + ' - ' + month_data)
+
+        $('.tem_sens').html(tem_sens);
+        $('.hum_sens').html(hum_sens);
+        $('.press_sens').html(press_sens);
         $('.air_sens').html(air_sens);
-
-
 
     }, 2000);
 
